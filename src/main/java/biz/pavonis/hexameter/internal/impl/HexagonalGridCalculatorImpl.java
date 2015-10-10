9a6a4a58b3f5959bdbcf5acc9c60ba069727f613
migepatschen <migepatschen@gmail.com>
2015-10-10 04:24:25 +0200

    Update HexagonalGridCalculatorImpl.java
    Added three pathfinding implementations

--- src/main/java/biz/pavonis/hexameter/internal/impl/HexagonalGridCalculatorImpl.java
+++ src/main/java/biz/pavonis/hexameter/internal/impl/HexagonalGridCalculatorImpl.java
@@ -42,4 +42,117 @@ public final class HexagonalGridCalculatorImpl implements
 		}
 		return ret;
 	}
+	
+	private class HexIntComparator implements Comparator<Hexagon> {
+		@Override
+		public int compare(Hexagon o1, Hexagon o2) {
+			Integer prio1 = (Integer) o1.getSatelliteData();
+			Integer prio2 = (Integer) o2.getSatelliteData();
+			return prio1.compareTo(prio2);
+		}
+	}
+
+	@Override
+	public List<Hexagon> calculatePath(Hexagon start, Hexagon end, PathfindingAlgorithm algorithm) {
+		Map<Hexagon, Hexagon> cameFrom;
+		switch (algorithm) {
+		case A_Star:
+			cameFrom = aStar(start, end);
+			break;
+		case BreadthFirst:
+			cameFrom = breadthFirst(start, end);
+			break;
+		case GreedyBreadthFirst:
+			cameFrom = greedyBestFirst(start, end);
+			break;
+		default:
+			return null;
+		}
+
+		Hexagon current = end;
+		List<Hexagon> path = new LinkedList<Hexagon>();
+		path.add(current);
+		while (!current.equals(start)) {
+			current = cameFrom.get(current);
+			path.add(current);
+		}
+		Collections.reverse(path);
+		return path;
+	}
+
+	private Map<Hexagon, Hexagon> aStar(Hexagon start, Hexagon end) {
+		PriorityQueue<Hexagon> frontier = new PriorityQueue<Hexagon>(new HexIntComparator());
+		start.setSatelliteData(new Integer(0));
+		frontier.add(start);
+		Map<Hexagon, Hexagon> cameFrom = new HashMap<Hexagon, Hexagon>();
+		cameFrom.put(start, null);
+		Map<Hexagon, Integer> costSoFar = new HashMap<Hexagon, Integer>();
+		costSoFar.put(start, 0);
+
+		while (!frontier.isEmpty()) {
+			Hexagon current = frontier.poll();
+			if (current.equals(end)) {
+				break;
+			}
+			Set<Hexagon> neigbours = calculateMovementRangeFrom(current, 1);
+			for (Hexagon next : neigbours) {
+				int newCost = costSoFar.get(current) + calculateDistanceBetween(current, next);
+				if ((!costSoFar.containsKey(next)) || newCost < costSoFar.get(next)) {
+					costSoFar.put(next, newCost);
+					int prio = calculateDistanceBetween(end, next);
+					next.setSatelliteData(new Integer(prio));
+					frontier.add(next);
+					cameFrom.put(next, current);
+				}
+			}
+		}
+		return cameFrom;
+	}
+
+	private Map<Hexagon, Hexagon> breadthFirst(Hexagon start, Hexagon end) {
+		Queue<Hexagon> frontier = new LinkedList<Hexagon>();
+		frontier.add(start);
+		Map<Hexagon, Hexagon> cameFrom = new HashMap<Hexagon, Hexagon>();
+		cameFrom.put(start, null);
+
+		while (!frontier.isEmpty()) {
+			Hexagon current = frontier.poll();
+			if (current.equals(end)) {
+				break;
+			}
+			Set<Hexagon> neigbours = calculateMovementRangeFrom(current, 1);
+			for (Hexagon next : neigbours) {
+				if (!cameFrom.containsKey(next)) {
+					frontier.add(next);
+					cameFrom.put(next, current);
+				}
+			}
+		}
+		return cameFrom;
+	}
+
+	private Map<Hexagon, Hexagon> greedyBestFirst(Hexagon start, Hexagon end) {
+		PriorityQueue<Hexagon> frontier = new PriorityQueue<Hexagon>(new HexIntComparator());
+		start.setSatelliteData(new Integer(0));
+		frontier.add(start);
+		Map<Hexagon, Hexagon> cameFrom = new HashMap<Hexagon, Hexagon>();
+		cameFrom.put(start, null);
+
+		while (!frontier.isEmpty()) {
+			Hexagon current = frontier.poll();
+			if (current.equals(end)) {
+				break;
+			}
+			Set<Hexagon> neigbours = calculateMovementRangeFrom(current, 1);
+			for (Hexagon next : neigbours) {
+				if (!cameFrom.containsKey(next)) {
+					int prio = calculateDistanceBetween(end, next);
+					next.setSatelliteData(new Integer(prio));
+					frontier.add(next);
+					cameFrom.put(next, current);
+				}
+			}
+		}
+		return cameFrom;
+	}
 }
