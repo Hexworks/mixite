551feddd84bd246bab3a429b23a56feda6a3d1e1
adam-arold <arold.adam@gmail.com>
2015-12-22 22:17:54 +0100

    cleanup

---
+++ hexameter-swt-example/src/main/java/org/codetome/hexameter/swtexample/SatelliteData.java
@@ -0,0 +1,15 @@
+package org.codetome.hexameter.swtexample;
+
+public class SatelliteData {
+
+    private boolean isSelected;
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean isSelected) {
+        this.isSelected = isSelected;
+    }
+
+}
