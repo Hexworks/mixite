f1ff48908c5cf0011f0e67040376807407a67348
adam-arold <arold.adam@gmail.com>
2016-01-13 22:29:46 +0100

    adding hexameter-util module

---
+++ hexameter-util/src/main/java/org/codetome/hexameter/util/MapUtil.java
@@ -0,0 +1,13 @@
+package org.codetome.hexameter.util;
+
+import org.codetome.hexameter.core.api.HexagonalGrid;
+
+/**
+ * This class can be used to Import/Export maps from/to hexameter
+ */
+public class MapUtil {
+
+    public static String exportMap(HexagonalGrid grid, String name, String tilesetUrl) {
+        return null;
+    }
+}
---
+++ hexameter-util/src/test/java/org/codetome/hexameter/util/MapUtilTest.java
@@ -0,0 +1,11 @@
+package org.codetome.hexameter.util;
+
+import org.junit.Test;
+
+public class MapUtilTest {
+
+    @Test
+    public void shouldReturnProperJsonWhenExportIsCalled() {
+    }
+
+}
