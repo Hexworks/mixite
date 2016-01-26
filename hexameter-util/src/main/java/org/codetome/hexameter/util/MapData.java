d942345b870225ee7ce3c0407726561a11c623cd
adam-arold <arold.adam@gmail.com>
2016-01-27 00:13:35 +0100

    adding lombok to the util project

--- hexameter-util/src/main/java/org/codetome/hexameter/util/MapData.java
+++ hexameter-util/src/main/java/org/codetome/hexameter/util/MapData.java
@@ -1,5 +1,7 @@
 package org.codetome.hexameter.util;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.codetome.hexameter.core.api.HexagonOrientation;
 import org.codetome.hexameter.core.api.HexagonalGridLayout;
 
@@ -9,85 +11,20 @@ import java.util.List;
 /**
  * Value class representing a map.
  */
+@Getter
+@Setter
+@SuppressWarnings("PMD.UnusedPrivateField")
 public class MapData {
+
     private String name;
     private String tilesetUrl;
     private String version;
-
     private int width;
     private int height;
     private double radius;
     private HexagonOrientation orientation;
     private HexagonalGridLayout layout;
     private List<String[]> cells = new ArrayList<>();
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTilesetUrl() {
-        return tilesetUrl;
-    }
-
-    public void setTilesetUrl(String tilesetUrl) {
-        this.tilesetUrl = tilesetUrl;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public double getRadius() {
-        return radius;
-    }
-
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
-
-    public HexagonOrientation getOrientation() {
-        return orientation;
-    }
-
-    public void setOrientation(HexagonOrientation orientation) {
-        this.orientation = orientation;
-    }
-
-    public HexagonalGridLayout getLayout() {
-        return layout;
-    }
-
-    public void setLayout(HexagonalGridLayout layout) {
-        this.layout = layout;
-    }
-
-    public List<String[]> getCells() {
-        return cells;
-    }
 
     public void addCellData(String[] cellData) {
         cells.add(cellData);
--- hexameter-util/src/main/java/org/codetome/hexameter/util/MapSatelliteData.java
+++ hexameter-util/src/main/java/org/codetome/hexameter/util/MapSatelliteData.java
@@ -1,18 +1,15 @@
 package org.codetome.hexameter.util;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.codetome.hexameter.core.api.AbstractSatelliteData;
 
+@Getter
+@Setter
+@SuppressWarnings("PMD.UnusedPrivateField")
 public class MapSatelliteData extends AbstractSatelliteData {
 
     private static final long serialVersionUID = -5144747914653122448L;
 
     private String tilesetId;
-
-    public String getTilesetId() {
-        return tilesetId;
-    }
-
-    public void setTilesetId(String tilesetId) {
-        this.tilesetId = tilesetId;
-    }
 }
