9a00f4836318e471a7689ec97bdf6eeaad5837d2
adam-arold <arold.adam@gmail.com>
2016-08-10 22:58:01 +0200

    adding rest project skeleton

---
+++ hexameter-rest/src/main/java/org/codetome/hexameter/rest/HexameterRestApplication.java
@@ -0,0 +1,12 @@
+package org.codetome.hexameter.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexameterRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexameterRestApplication.class, args);
+	}
+}
